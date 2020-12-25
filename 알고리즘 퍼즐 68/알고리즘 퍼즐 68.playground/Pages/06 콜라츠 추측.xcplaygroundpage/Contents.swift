2e4c01d231e858f63005e8ae73c1ffb57df2dd17
Song Lee <songlish233@gmail.com>
2020-12-25 23:09:36 +0900

    Quiz06 콜라츠 추측 done

--- 알고리즘 퍼즐 68/알고리즘 퍼즐 68.playground/Pages/06 콜라츠 추측.xcplaygroundpage/Contents.swift
+++ 알고리즘 퍼즐 68/알고리즘 퍼즐 68.playground/Pages/06 콜라츠 추측.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@
  - n이 홀수인 경우, n에 3을 곱해 1을 더한다.
  위의 계산을 반복하면 초깃값이 어떤 수였더라도 반드시 1에 도달한다.
  
- 이 내용을 조금 바꾸어 초깃값이 짝수면 맨 처음에만 n에 3을 곱하여 1일 더하는 것에서 시작하기로 하고, '맨 처음의 수'로 돌아가는 법을 생각해봅시다.
+ 이 내용을 조금 바꾸어 초깃값이 짝수면 맨 처음에만 n에 3을 곱하여 1을 더하는 것에서 시작하기로 하고, '맨 처음의 수'로 돌아가는 법을 생각해봅시다.
  
  예를 들어 2로 시작하는 경우에는 다음과 같습니다.
  2 -> 7 -> 22 -> 11 -> 34 -> 17 -> 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2
@@ -28,11 +28,52 @@ func collatz(_ startNum: Int,_ numberNow: Int,_ count: Int) -> Int {
     if numberNow == 1 {
         return 0
     } else if numberNow % 2 == 0 {
-        return 1 + collatz(startNum, numberNow/2, count+1)
+        return 1 + collatz(startNum, numberNow / 2, count+1)
     } else {
-        return 1 + collatz(startNum, numberNow*3+1, count+1)
+        return 1 + collatz(startNum, numberNow * 3 + 1, count+1)
     }
 }
 
 let n = 10
 print("\(n)는 콜라츠 추측에 따르면 계산 \(collatz(n, n, 0))번만에 1이 된다")
+
+
+
+//재귀로 구현
+func backToStartNum(_ startNum: Int,_ numberNow: Int,_ count: Int) -> Bool {
+    if numberNow == startNum && count != 0 {
+        return true
+    } else if numberNow == 1 {
+        return false
+    } else if count == 0 || numberNow % 2 == 1{
+        return backToStartNum(startNum, numberNow * 3 + 1, count+1)
+    } else {
+        return backToStartNum(startNum, numberNow / 2, count+1)
+    }
+}
+
+var answer = 0
+for number in 1...5000 {
+    if backToStartNum(number*2, number*2, 0) { answer += 1 }
+}
+print("정답은 \(answer)개")
+
+
+//반복문으로 구현
+func backToStartNum2(startNum: Int) -> Bool {
+    
+    var numberNow = startNum * 3 + 1
+    
+    while numberNow != 1, numberNow != startNum {
+        let remainder = numberNow % 2
+        numberNow = remainder == 0 ? numberNow/2 : numberNow * 3 + 1
+    }
+    
+    return numberNow == 1 ? false : true
+}
+
+var answer2 = 0
+for number in 1...5000 {
+    if backToStartNum2(startNum: number * 2) { answer2 += 1 }
+}
+print("정답은 \(answer2)개")
